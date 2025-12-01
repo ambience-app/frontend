24281ba5bad179dd269c2ddfbab4e17bc3c04d41
DeborahOlaboye <deboraholaboye@gmail.com>
2025-12-01 01:58:23 +0100

    Create Profile Components and Hooks

---
+++ src/types/profile.ts
@@ -0,0 +1,11 @@
+export interface Profile {
+  address: string;
+  ensName?: string;
+  avatar?: string;
+  bio?: string;
+  website?: string;
+  twitter?: string;
+  discord?: string;
+  joinedDate?: Date;
+  lastSeen?: Date;
+}
